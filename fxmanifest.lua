615f8f8ee7e4d6bd89c3654d84fa0cebd9ad5c0e
LegitimatelyRyan <57320978+LegitimatelyRyan@users.noreply.github.com>
2024-10-31 15:17:15 -0400

    add fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version("cerulean")
+game("gta5")
+lua54("yes")
+author("couldthisberyan")
+description("Provides public works related RP functions to QB-Core")
+version("0.1")
+
+shared_scripts({
+	"",
+})
+
+client_scripts({
+	"",
+})
+
+server_scripts({
+	"",
+})
+
+files({
+	"",
+})
