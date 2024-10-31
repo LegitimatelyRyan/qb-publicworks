b025b42599089b366799b33a8791f1608e666ff8
TheBaited <102853406+TheBaited@users.noreply.github.com>
2024-10-31 22:51:44 +0000

    Re-written and began on iteration to break more lights.

--- server/server.lua
+++ server/server.lua
@@ -5,7 +5,7 @@ local BrokenSLights = {}
 local QBCore = exports["qb-core"]:GetCoreObject()
 
 -- Helper function to check if an object is a traffic light
-function IsTrafficLight(entity)
+local function IsTrafficLight(entity)
 	-- Get the hash model of the entity
 	local model = entity
 
@@ -13,16 +13,13 @@ function IsTrafficLight(entity)
 	local trafficLightModels = Config.TrafficLights
 
 	-- Check if the entity's model is a traffic light
-	for _, trafficLightHash in ipairs(trafficLightModels) do
-		if model == trafficLightHash then
-			return true
-		end
+	if lib.table.contains(trafficLightModels, model) then
+		return true
 	end
-
 	return false
 end
 
-function IsStreetLight(entity)
+local function IsStreetLight(entity)
 	-- Get the hash model of the entity
 	local model = GetEntityModel(entity)
 
@@ -30,51 +27,73 @@ function IsStreetLight(entity)
 	local streetLightModels = Config.StreetLights
 
 	-- Check if the entity's model is a traffic light
-	for _, trafficLightHash in ipairs(trafficLightModels) do
-		if model == trafficLightHash then
-			return true
-		end
+	if lib.table.contains(streetLightModels, model) then
+		return true
 	end
-
 	return false
 end
 
--- Enumerator function to loop through all objects
-function EnumerateObjects()
-	return coroutine.wrap(function()
-		local handle, object = FindFirstObject()
-		if not handle or handle == -1 then
-			return
+local function BreakTrafficLight()
+	-- Selects a random traffic light from the server.
+	local selectedLight = TrafficLights[math.random(#TrafficLights)]
+
+	-- Sets this traffic light to have no lighting.
+	SetEntityTrafficlightOverride(selectedLight, 3)
+
+	-- Adds the data to the table to allow it to be fixed in the future.
+	BrokenTLights[#BrokenTLights + 1] = {
+		["entity"] = selectedLight,
+		["coords"] = GetEntityCoords(selectedLight),
+	}
+
+	-- TODO: Dispatch Bridge to send to the correct authorities.
+end
+
+local function BreakStreetLight()
+	-- Selects a random street light from the server.
+	local selectedLight = StreetLights[math.random(#StreetLights)]
+
+	-- Sets this street light to have no lighting.
+	SetEntityTrafficlightOverride(selectedLight, 3)
+
+	-- Adds the data to the table to allow it to be fixed in the future.
+	BrokenSLights[#BrokenSLights + 1] = {
+		["entity"] = selectedLight,
+		["coords"] = GetEntityCoords(selectedLight),
+	}
+
+	-- TODO: Dispatch Bridge to send to the correct authorities.
+end
+
+-- Loops through all objects to check models.
+Citizen.CreateThread(function()
+	-- When the server starts, it will run this loop first.
+	for _, object in pairs(GetAllObjects()) do
+		local hash = GetEntityModel(object)
+
+		-- Check if the object is a traffic light, if so adds it to a table which will be iterated through later.
+		if IsTrafficLight(hash) then
+			TrafficLights[#TrafficLights + 1] = hash
 		end
 
-		local success
-		repeat
-			coroutine.yield(object)
-			success, object = FindNextObject(handle)
-		until not success
-
-		EndFindObject(handle)
-	end)
-end
-
-for entity in EnumerateObjects() do
-	if IsTrafficLight(entity) then
-		table.insert(TrafficLights, entity)
-	elseif IsStreetLight(entity) then
-		table.insert(StreetLights, entity)
+		-- Checks if the object is a street light, if so adds it to a table which will be iterated through later.
+		if IsStreetLight(hash) then
+			StreetLights[#StreetLights + 1] = hash
+		end
 	end
-end
 
-function BreakTrafficLight(entity)
-	if
-		IsTrafficLight(entity)
-		and Config.BreakTrafficLights
-		and #BrokenTLights < Config.MaxTrafficLightsOut
-		and not BrokenTLights[entity]
-	then
-		print("We've chosen a traffic light at " .. GetEntityCoords(entity))
-		table.insert(BrokenTLights, entity) -- add the entity to our registry
-		-- tell the client the lights out
-		-- do dispatching?!?!?!
+	-- Once the objects have been retrieved it will then start this timed loop.
+	while true do
+		-- If broken traffic lights are less than the config max then it will break a street light.
+		if Config.MaxTrafficLightsOut < #BrokenTLights then
+			BreakTrafficLight()
+		end
+
+		-- If broken street lights are less than the config max then it will break a street light.
+		if Config.MaxStreetLightsOut < #BrokenSLights then
+			BreakStreetLight()
+		end
+
+		Wait(5000)
 	end
-end
+end)
