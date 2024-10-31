eb14375d722954d8f1b0c6c7da7389fe71350e9e
TheBaited <102853406+TheBaited@users.noreply.github.com>
2024-10-31 22:52:20 +0000

    Added Config.MaxStreetLightsOut and hashes for traffic light props

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = {}
 
 --## JOB CONFIGURATIONS ##--
 Config.MaxTrafficLightsOut = 1 -- Maximum number of traffic lights that can be out at any given time
+Config.MaxStreetLightsOut = 1 -- Maximum number of street lights that can be out at any given time
 Config.TimeoutBeforeLightsClear = 30 -- Time in minutes before a call "clears" aka gets set back to working
 Config.BreakTrafficLights = true
 Config.BreakStreetLights = true
@@ -13,8 +14,11 @@ Config.QBJobs = {
 }
 
 Config.TrafficLights = {
-	0x83838863, -- Small traffic light model
-	0xB8A15DAB, -- Large traffic light model
+	`prop_traffic_01a`,
+	`prop_traffic_01b`,
+	`prop_traffic_01d`,
+	`prop_traffic_03a`,
+	`prop_traffic_lightset_01`,
 }
 
 Config.StreetLights = {
