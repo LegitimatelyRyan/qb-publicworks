9061d6fefc307015b270926be6c4b14c962ed4b6
LegitimateIyRyan <57320978+LegitimatelyRyan@users.noreply.github.com>
2024-11-01 09:45:24 -0400

    insert streetlight is added

--- config.lua
+++ config.lua
@@ -23,6 +23,32 @@ Config.TrafficLights = {
 }
 
 Config.StreetLights = {
-	0x83838863, -- Small traffic light model
-	0xB8A15DAB, -- Large traffic light model
+	`prop_streetlight_07a`,
+	`prop_streetlight_11a`,
+	`prop_streetlight_05`,
+	`prop_streetlight_11c`,
+	`prop_streetlight_12b`,
+	`prop_streetlight_06`,
+	`prop_streetlight_03d`,
+	`prop_streetlight_04`,
+	`prop_streetlight_14a`,
+	`prop_streetlight_16a`,
+	`prop_streetlight_05_b`,
+	`prop_streetlight_12a`,
+	`prop_streetlight_03e`,
+	`prop_streetlight_07b`,
+	`prop_streetlight_15a`,
+	`prop_streetlight_01b`,
+	`prop_streetlight_08`,
+	`prop_streetlight_01`,
+	`prop_streetlight_11b`,
+	`prop_streetlight_10`,
+	`prop_streetlight_03b`,
+	`prop_streetlight_03c`,
+	`prop_streetlight_03`,
+	`prop_streetlight_02`,
+	`prop_streetlight_09`,
+	`prop_snow_streetlight01`,
+	`prop_snow_streetlight_09`,
+	`prop_snow_streetlight_01_frag_`,
 }
